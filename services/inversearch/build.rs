4fadcb5ca9f91a03c55d6e61dbe87f903e7881e6
Admin <3353074868@qq.com>
2026-02-04 23:46:09 +0800

    update depi

--- services/inversearch/build.rs
+++ services/inversearch/build.rs
@@ -1,7 +1,9 @@
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let proto_file = "./proto/inversearch.proto";
 
     tonic_build::configure()
         .build_server(true)
         .compile(&[proto_file], &["proto/"])?;
+
+    Ok(())
 }
